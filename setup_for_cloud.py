83f15c55f7a7ea3be0e4e8786bc6d78b0ba71578
Brandon Mai <anhbinhbao0@gmail.com>
2025-06-18 20:23:11 +0700

    fix: remove folder creation

--- setup_for_cloud.py
+++ setup_for_cloud.py
@@ -4,32 +4,6 @@ import sys
 import os
 
 def setup_for_cloud():
-    """Complete setup for Colab/Kaggle environments"""
-    
-    # Environment detection and path setup
-    if 'google.colab' in sys.modules:
-        print("Setting up for Google Colab...")
-        from google.colab import drive
-        drive.mount('/content/drive')
-        base_path = '/content/drive/MyDrive/othello/'
-    elif '/kaggle' in sys.modules or os.path.exists('/kaggle'):
-        print("Setting up for Kaggle... Remember to copy checkpoint from dataset directory to working directory.")
-        base_path = '/kaggle/working/'
-    else:
-        print("Running locally...")
-        base_path = './'
-    
-    # Update config paths
-    import config
-    config.checkpoint_path = base_path + 'checkpoints/'
-    config.data_path = base_path + 'data/'
-    config.log_path = base_path + 'logs/'
-    
-    # Create directories
-    for path in [config.checkpoint_path, config.data_path, config.log_path]:
-        os.makedirs(path, exist_ok=True)
-    
-    # GPU/TPU setup
     try:
         tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
         tf.config.experimental_connect_to_cluster(tpu)
